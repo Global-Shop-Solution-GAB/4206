868065d59d4b0db90d77577619a18b3ed9693e67
Service Web <sw@gssmail.com>
2024-03-11 13:40:14 -0500

    Commit changes in C:\Websites\Git\emarlow\4206\GCG_4206_BIOPNOrder.sql

---
+++ GCG_4206_BIOPNOrder.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "GCG_4206_BIOPNORDER"(
+ "BIRUNID" INTEGER,
+ "Customer" VARCHAR(6),
+ "Customer_Name" VARCHAR(30),
+ "Customer_PO" VARCHAR(15),
+ "Order_No" VARCHAR(7),
+ "Order_Ln" VARCHAR(4),
+ "Part" VARCHAR(20),
+ "Location" VARCHAR(2),
+ "Description" VARCHAR(30),
+ "Uom" VARCHAR(2),
+ "SO_qty" FLOAT,
+ "Item_promise_dt" DATE,
+ "Qty_order" FLOAT,
+ "Qty_onhand" FLOAT);
+ 
